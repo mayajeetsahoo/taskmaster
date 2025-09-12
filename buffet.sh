eeb70c15e42b246ff073c920d5a70f28a3756109
mayajeetsahoo <mayajeetsahoo26@gmail.com>
2025-09-12 19:55:31 +0530

    f

--- buffet.sh
+++ buffet.sh
@@ -1,5 +1,5 @@
 python3 buffet.py \
     --model meta-llama/Llama-2-7b-hf\
-    --auth_token  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\
+    --auth_token  \
     --sparsity 0.4\
     --sparsity_allocation 1
