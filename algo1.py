291fdeaf3fa4324aa946b747421e2198d637d6de
mayajeetsahoo <mayajeetsahoo26@gmail.com>
2025-09-12 19:37:04 +0530

    updating algo1

--- algo1.py
+++ algo1.py
@@ -1,19 +1,17 @@
+import argparse
+import logging
+
 from lm_eval import evaluator
 from lm_eval.models.huggingface import HFLM
 from lm_eval.utils import make_table
-
-import argparse
-from typing import Tuple
-import logging
 import torch
-import torch.nn as nn
-import numpy as np
-from transformers import LlamaTokenizer, GenerationConfig, LlamaConfig, AutoTokenizer, AutoModelForCausalLM
-from transformers.models.llama.modeling_llama import LlamaConfig, LlamaDecoderLayer, LlamaForCausalLM, LlamaRMSNorm
+from transformers import LlamaTokenizer, AutoTokenizer, AutoModelForCausalLM
+from transformers.models.llama.modeling_llama import LlamaForCausalLM
+
 import data_utils
 import utils
-from tqdm import tqdm
-from gs import functor_gs , global_sparsity_allocation
+from gs import   global_sparsity_allocation
+from algo1_modular import mode_gpt_algo1
 
 logging.basicConfig(
     level=logging.INFO,
@@ -28,7 +26,7 @@ parser.add_argument(
         "--cal-dataset",
         type=str,
         help="Dataset to calibrate and calculate perplexity on.",
-        choices=["wikitext2", "ptb", "c4", "alpaca","glue","hotpot","pubmed","medqa_4options","billsum","multilegalpile"],
+        choices=["wikitext2", "ptb", "c4", "alpaca","glue","hotpot","pubmedqa","medqa_4options","billsum","multilegalpile","mathqa","sciq"],
         default="wikitext2",
     )
 parser.add_argument(
@@ -39,7 +37,7 @@ parser.add_argument(
 )
 
 
-parser.add_argument("--cal-batch-size", type=int, default=16, help="Batch size for loading the calibration data.")
+parser.add_argument("--cal-batch-size", type=int, default=8, help="Batch size for loading the calibration data.")
 parser.add_argument("--cal-max-seqlen", type=int, default=1024, help="Maximum sequence length for the calibration data.")
 parser.add_argument("--varied-seqlen", action="store_true", help="Varied sequence lengths in the calibration data.")
 parser.add_argument("--seed", type=int, default=42, help="Seed for sampling the calibration data.")
@@ -70,18 +68,14 @@ elif args.model.split('/')[0] == "Qwen":
     model = AutoModelForCausalLM.from_pretrained(args.model, trust_remote_code=True, torch_dtype=torch.float32).to(torch.device("cuda:0"))
     model.config.head_dim = model.config.hidden_size//model.config.num_attention_heads
 
-device = model.device
 
-
-
-
-if args.cal_dataset not in ["billsum","multilegalpile"]:
+if args.cal_dataset not in ["billsum","multilegalpile","pubmedqa"]:
     dataset = data_utils.get_dataset(args.cal_dataset)
 
 if args.cal_dataset == "hotpot":
     train_dataset, test_dataset = dataset["train"], dataset["validation"]
-elif args.cal_dataset == "pubmed":
-    train_dataset, test_dataset = dataset["train"], dataset["train"]
+elif args.cal_dataset == "pubmedqa":
+    train_dataset, test_dataset = data_utils.get_dataset(args.cal_dataset)
 elif args.cal_dataset == "medqa_4options":
     train_dataset, test_dataset = dataset["train"], dataset["validation"]
 elif args.cal_dataset == "billsum":
@@ -90,7 +84,6 @@ elif args.cal_dataset == "multilegalpile":
     train_dataset, test_dataset = data_utils.get_dataset(args.cal_dataset)
 else:
     train_dataset, test_dataset = dataset["train"], dataset["test"]
-# train_dataset, test_dataset = dataset["train"], dataset["test"]
 train_loader = data_utils.prepare_dataloader(
     dataset=train_dataset,
     tokenizer=tokenizer,
@@ -114,13 +107,13 @@ data = {k: v.to("cuda:0") for k, v in data.items()}
 
 param = sum(int(p.nelement()) for p in model.parameters())
 logging.info(f"unpruned model parameter count is :{param}")
-if args.cal_dataset not in ["pubmed","billsum","medqa_4options"]:
+if args.cal_dataset not in ["pubmedqa","billsum","medqa_4options","mathqa","sciq"]:
     logging.info("calculating unpruned model perplexity")
-    # dataset_ppl = utils.evaluate_ppl(model, model.config.pad_token_id, test_loader)
-    # logging.info(f"unpruned model perplexity is :{dataset_ppl}")
+    dataset_ppl = utils.evaluate_ppl(model, model.config.pad_token_id, test_loader)
+    logging.info(f"unpruned model perplexity is :{dataset_ppl}")
 
 
-if args.cal_dataset in ["pubmed","medqa_4options"]:
+if args.cal_dataset in ["pubmedqa","medqa_4options","mathqa","sciq"]:
     logging.info(f"calculating unpruned model accuracy for medical question answering")
     lm = HFLM(pretrained=model, tokenizer=tokenizer)
     results = evaluator.simple_evaluate(
@@ -133,12 +126,12 @@ if args.cal_dataset in ["pubmed","medqa_4options"]:
 if args.cal_dataset == "billsum":
     logging.info("calculating unpruned model rouge scores")
     rouge_scores = utils.evaluate_rouge(model,test_dataset,tokenizer)
-    logging.info(f"unpruned model rouge scores are :rouge1:{rouge_scores["rouge1"]} rouge2:{rouge_scores["rouge2"]}  rougeL:{rouge_scores["rougeL"]}")
+    logging.info(f"unpruned model rouge scores are :rouge1:{rouge_scores['rouge1']} rouge2:{rouge_scores['rouge2']}  rougeL:{rouge_scores['rougeL']}")
 
 logging.info(f"calculating optimal sparsity allocation")
 if args.sparsity_allocation ==1:
     gs = global_sparsity_allocation(model, data, args.sparsity)
-    # sparsity_layer = gs.gs
+    sparsity_layer = gs.gs
     sparsity_layer = [0.8 if i>1 else (i.item()) for i in gs.gs]
 else:
     sparsity_layer = []
@@ -147,85 +140,22 @@ else:
 
 logging.info(f"calculating optimal sparsity allocation completed")
 
-class InterruptExecution(Exception):
-    pass
-
-class algo1_functor():
-    def __init__(self):
-        self.co_var = torch.zeros(model.config.intermediate_size, model.config.intermediate_size)
+# Intatiating Pruner
+pruner = mode_gpt_algo1()
     
-    def __call__(self,module,input,output):
-        gate = module.gate_proj(input[0])
-        up = module.up_proj(input[0])
-        hidden = torch.nn.functional.silu(gate) * up
-        transpose = torch.transpose(hidden,1,2)
-        mul = torch.matmul(transpose,hidden)
-
-        co_var = mul.sum(dim=0)/hidden.shape[0]
-        co_var = co_var.to(torch.float32).detach().cpu()
-        
-        self.co_var = self.co_var + co_var
-        raise InterruptExecution()
-    
-
-list_layers = list(model.model.layers.named_children())
-
-for layer in tqdm(range(model.config.num_hidden_layers)):
-    # print(f'compressing mlp layer of {layer} th decoder layer')
-
-    module = list_layers[layer][1].mlp
-    name = list_layers[layer][0]
-
-    module._name = name
-    functor = algo1_functor()
-    hook = module.register_forward_hook(functor)
-
-    for batch in train_loader:
-        batch.pop("labels")
-        with torch.no_grad():
-            try:
-                _ = model(**batch)
-            except InterruptExecution:
-                pass
-    import pdb;pdb.set_trace()
-    hook.remove()
-    ## pruning
-
-    torch.cuda.empty_cache()
-    co_var = functor.co_var
-
-    I = torch.eye(co_var.size(0), device=co_var.device, dtype=co_var.dtype)
-    A = (co_var+I)
-    score_mat = torch.matmul(co_var,torch.inverse(A))
-    scores_vector = torch.diagonal(score_mat)
-
-    topk_scores, topk_indices = torch.topk(scores_vector, int((1-sparsity_layer[int(module._name)])*co_var.size(0)), largest=True)
-    topk_indices = topk_indices.sort().values
-    d_int = scores_vector.shape[0]
-    S_k = torch.zeros((d_int, int((1-sparsity_layer[int(module._name)])*co_var.size(0))), dtype=torch.float32)
-    S_k[topk_indices, torch.arange(int((1-sparsity_layer[int(module._name)])*co_var.size(0)))] = 1.0
-    W_U_k = torch.concat([module.up_proj.weight,module.gate_proj.weight],dim=1).T.to(torch.float32) @ S_k.to(module.gate_proj.weight.device) 
-
-    Mat = (S_k.T @ co_var @ S_k).to(torch.float64)
-    W_D_k = torch.linalg.pinv(Mat).to(torch.float32) @ (S_k.T @ co_var @ module.down_proj.weight.data.T.cpu())
-    
-    new_up_proj = W_U_k.T[:,:model.config.hidden_size]
-    new_gate_proj = W_U_k.T[:,model.config.hidden_size:]
-    module.up_proj.weight.data = new_up_proj.to(module.up_proj.weight.device)
-    module.gate_proj.weight.data = new_gate_proj.to(module.gate_proj.weight.device)
-
-    module.down_proj.weight.data = W_D_k.T.to(module.down_proj.weight.device)
+#Pruning
+pruner.prune(model,train_loader, sparsity_layer)
 
 
 param = sum(int(p.nelement()) for p in model.parameters())
 logging.info(f"pruned model parameter count is :{param}")
-if args.cal_dataset not in ["pubmed","billsum","medqa_4options"]:
+if args.cal_dataset not in ["pubmedqa","billsum","medqa_4options","mathqa","sciq"]:
     logging.info("calculating pruned model perplexity")
     dataset_ppl = utils.evaluate_ppl(model, model.config.pad_token_id, test_loader)
     logging.info(f"pruned model perplexity is :{dataset_ppl}")
 
 
-if args.cal_dataset in ["pubmed","medqa_4options"]:
+if args.cal_dataset in ["pubmedqa","medqa_4options","mathqa","sciq"]:
     logging.info(f"calculating pruned model accuracy for medical question answering")
     lm = HFLM(pretrained=model, tokenizer=tokenizer)
     results = evaluator.simple_evaluate(
@@ -239,6 +169,6 @@ if args.cal_dataset in ["pubmed","medqa_4options"]:
 if args.cal_dataset == "billsum":
     logging.info("calculating pruned model rouge scores")
     rouge_scores = utils.evaluate_rouge(model,test_dataset,tokenizer)
-    logging.info(f"pruned model rouge scores are :rouge1:{rouge_scores["rouge1"]} rouge2:{rouge_scores["rouge2"]}  rougeL:{rouge_scores["rougeL"]}")
+    logging.info(f"pruned model rouge scores are :rouge1:{rouge_scores['rouge1']} rouge2:{rouge_scores['rouge2']}  rougeL:{rouge_scores['rougeL']}")
 
 
